c617ab7465417af869656b1e5bd383bbd4976a0b
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-07-23 00:08:34 -0400

    Experiment 06 Program 02.cpp

---
+++ Experiment 06 Program 02.cpp
@@ -0,0 +1,11 @@
+//Printing any same word five times
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        cout<<"Symbiosis Institute of Technology("<<i+1<<")"<<endl;
+    }
+}
