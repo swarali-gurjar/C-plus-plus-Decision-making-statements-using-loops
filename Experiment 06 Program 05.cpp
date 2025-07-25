4e1d4ce428392a975b6d52c51fd7d228656bb9cc
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-07-25 11:05:31 +0530

    Experiment 06 Program 05.cpp

---
+++ Experiment 06 Program 05.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Outer loop
+for (int i=1;i<=2;i++)
+{
+    cout<<"Outer:"<<i<<"\n";//Executes 2 times
+    //Inner loop
+    for (int j=1;j<=3;j++)
+    {
+        cout<<"Inner:"<<j<<"\n";//Executes 3 times
+    }
+}
+}
