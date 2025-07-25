b9f24f6959aa589c39a5370895c49e01b7782f42
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-07-25 11:59:13 +0530

    Experiment 06 Program 09.cpp

--- Experiment 06 Program 09.cpp
+++ Experiment 06 Program 09.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
     int n=4;
-    int beta=65;
+    int ch=65;
     int i,j;
     for (i=1;i<=n;i++)
     {
         for (j=1;j<=i;j++)
         {
-            cout<<char(beta)<<" ";
-            beta++;
+            cout<<char(ch)<<" ";
+            ch++;
         }
         cout<<endl;
     }
