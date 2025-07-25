653f8aec16c007c962ca07ffc346272a3217e165
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-07-25 11:27:37 +0530

    Experiment 06 Program 07.cpp

---
+++ Experiment 06 Program 07.cpp
@@ -0,0 +1,23 @@
+//Printing inverted stars pattern
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, k;
+    int n = 5;
+
+    for (i = 1; i <= n; i++) {
+    
+        for (k = 1; k<= n - i; k++) {
+            cout << "  ";
+        }
+
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
